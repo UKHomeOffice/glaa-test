b46368b9653ca83f1aadd5aeac5fd8ca0bb1ec27
MorayM <moray.macdonald1@gmail.com>
2017-12-19 17:13:04 +0000

    Fix scripts to off env vars

--- kubedb/db_setup.sh
+++ kubedb/db_setup.sh
@@ -1,2 +1,2 @@
-/opt/mssql-tools/bin/sqlcmd -S localhost -U "sa" -P $SA_PASSWORD -i db_setup.sql -v DB_USER = $DB_USER DB_PASS = $DB_PASS
+/opt/mssql-tools/bin/sqlcmd -S localhost -U "sa" -P $SA_PASSWORD -i db_setup.sql
 echo "Done"
